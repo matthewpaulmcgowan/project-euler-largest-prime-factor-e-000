487be23f29c5dd519046a7629065efe25cd6477d
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-11-23 00:11:43 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+require 'prime'
+def largest_prime_factor (input)
+a=input.prime_division
+b=[]
+  a.each do |i|
+    i.each do |t|
+      b.push(t)
+    end
+  end
+c=b.sort!
+  return c[c.length-1]
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,21 @@
 # Enter your object-oriented solution here!
+require 'prime'
+class LargestPrimeFactor
+  def initialize input
+    @input=input
+  end
+  def input
+    @input
+  end
+  def number
+a=@input.prime_division
+b=[]
+  a.each do |i|
+    i.each do |t|
+      b.push(t)
+    end
+  end
+c=b.sort!
+  return c[c.length-1]
+end
+end
